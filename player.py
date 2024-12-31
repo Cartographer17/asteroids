44d5eb8befb1040aa69b1fafad103a09f44b8ef4
Cartographer17 <cartographer17@tutanota.com>
2024-12-31 06:04:07 -0500

    I: 3-4, added shot rate

--- player.py
+++ player.py
@@ -7,13 +7,15 @@ from constants import PLAYER_SHOOT_SPEED
 
 class Player(CircleShape, pygame.sprite.Sprite):
     containers = None
-    def __init__(self,x,y,):
-        CircleShape.__init__(self,x,y,PLAYER_RADIUS)
+    def __init__(self, x, y):
+        CircleShape.__init__(self, x, y, PLAYER_RADIUS)
         pygame.sprite.Sprite.__init__(self)
-        self.rotation=0
+        self.rotation = 0
+        self.time = 0
+        self.shoot_allowed = True
         if self.containers:
             self.add(self.containers)
-    # in the player class
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -22,10 +24,15 @@ class Player(CircleShape, pygame.sprite.Sprite):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
-    def draw(self,screen):
+    def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def update(self, dt):
+        if self.time > 0:
+            self.time -= dt
+            if self.time <= 0:
+                self.shoot_allowed = True
+        
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotation += PLAYER_TURN_SPEED * dt
@@ -38,14 +45,17 @@ class Player(CircleShape, pygame.sprite.Sprite):
         if keys[pygame.K_SPACE]:
             self.shoot()
 
-    
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        new_shot = Shot(self.position.x, self.position.y)
-        shot_direction = pygame.Vector2(0, 1).rotate(self.rotation)
-        new_shot.velocity = shot_direction * PLAYER_SHOOT_SPEED
-        
+        if self.shoot_allowed:
+            new_shot = Shot(self.position.x, self.position.y)
+            shot_direction = pygame.Vector2(0, 1).rotate(self.rotation)
+            new_shot.velocity = shot_direction * PLAYER_SHOOT_SPEED
+
+            # Set the cooldown timer and disallow shooting
+            self.time = 0.3  # Assuming 0.3 seconds is your cooldown time
+            self.shoot_allowed = False
 
